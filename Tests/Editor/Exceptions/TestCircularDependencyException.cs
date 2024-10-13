7573d96a92a1f1ea5ec02272824b73e2a259e391
Alexander Saratow <a@saratow.biz>
2024-10-13 11:52:10 +0200

    Adjust CircularDependencyException message and add an according unit test

---
+++ Tests/Editor/Exceptions/TestCircularDependencyException.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace FrankenBit.BoltWire.Exceptions;
+
+public sealed class TestCircularDependencyException
+{
+    [Test]
+    public void Ctor_WithParams_ProvidesExpectedMessage()
+    {
+        Assert.That(new CircularDependencyException(typeof(ITestService), typeof(TestService)).Message,
+        Is.EqualTo("Recursive composition detected for ITestService service dependency TestService."));
+    }
+
+    private interface ITestService
+    {
+    }
+
+    private sealed class TestService : ITestService
+    {
+    }
+}
