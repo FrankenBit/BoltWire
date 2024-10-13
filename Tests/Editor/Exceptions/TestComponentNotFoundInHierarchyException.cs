d2d0e32fcf3da532080faf8f1bd76742a51bdebd
Alexander Saratow <a@saratow.biz>
2024-10-13 11:57:53 +0200

    Add unit test for ComponentNotFoundInHierarchyException

---
+++ Tests/Editor/Exceptions/TestComponentNotFoundInHierarchyException.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace FrankenBit.BoltWire.Exceptions;
+
+public sealed class TestComponentNotFoundInHierarchyException
+{
+    [Test]
+    public void Ctor_WithParams_ProvidesExpectedMessage()
+    {
+        Assert.That(ComponentNotFoundInHierarchyException.Create<TestService>().Message,
+        Is.EqualTo("Component of type TestService not found in hierarchy."));
+    }
+}
